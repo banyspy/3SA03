fb5b7f9d07bd4c57b0af1943c3c73b5009d4e27f
unknown <Hatyai cyber@HatyaiCyber.com>
2020-08-16 20:00:14 +0700

    Part7

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './App.css';
 import WordCard from './WordCard'
 
-const word = "Hello";
+const word = "Welcome";
 function App() {
   return (
     <div>
-    <WordCard value="hello"/>
+    <WordCard value="welcome"/>
     </div>
     );
   }
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,15 +1,8 @@
+  
 import React, { useState } from 'react';
 export default function CharacterCard(props) {
  const [active, setActive] = useState(false);
 
- const attemptRef = useRef(props.attempt);
- useEffect(() => {
-    if(attemptRef.current != props.attempt){
-    setActive(false)
-    attemptRef.current = props.attempt
-    }
-    })
-   
  const className = `card ${active ? 'activeCard': ''}`
  const activate = () => {
     if(!active){
